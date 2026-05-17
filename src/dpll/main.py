fcc3c2b6eef77b1e2d762e63409ce6b94ade65c6
Imogen <me@imogen.tech>
2026-05-17 13:17:03 +0200

    blep

--- src/dpll/main.py
+++ src/dpll/main.py
@@ -1,9 +1,11 @@
 import json
 from abc import ABC, abstractmethod
+from collections.abc import Iterable
 from dataclasses import dataclass, field
 from pathlib import Path
 from typing import Any, ClassVar
 from uuid import UUID, uuid4
+from zipfile import ZipFile
 
 type Json = dict[str, Any]
 
@@ -15,49 +17,62 @@ class Literal:
 
 
 def get_template(name: str) -> Json:
+    if not name[0].islower():
+        name = name[0].lower() + name[1:]
     path = Path(__file__).parent.joinpath("templates").joinpath(name).with_suffix(".json")
     return json.loads(path.read_text())
-
-
-def split_text(text: str) -> tuple[str, str]:
-    split = len(text) // 2
-    first: list[str] = []
-    second: list[str] = []
-    curr_len = 0
-    for word in text.split(" "):
-        curr_len += len(word) + 1
-        if curr_len <= split:
-            first.append(word)
-        else:
-            second.append(word)
-    return " ".join(first), " ".join(second)
 
 
 def format_text(text: str) -> str:
     return "".join(f"<p>{line}</p>" for line in text.splitlines())
 
 
-@dataclass
-class Question:
-    title: str
-    subcontent_id: UUID = field(default_factory=uuid4, init=False)
-    index: int | None = field(default=None, init=False)
+def iter_children(path: Path) -> Iterable[Path]:
+    for child in path.iterdir():
+        if child.is_file():
+            yield child
+        elif child.is_dir():
+            yield from iter_children(child)
+        else:
+            raise ValueError
 
-    template: ClassVar[str]
+
+def bundle_template(out: Path) -> None:
+    if out.exists():
+        out.unlink()
+    template = Path(__file__).parent / "templates" / "template"
+    with ZipFile(out, mode="w", compresslevel=9) as zip:
+        for file in iter_children(template):
+            zip.write(file, file.relative_to(template))
+
+
+@dataclass
+class Element:
+    subcontent_id: UUID = field(default_factory=uuid4, init=False)
 
     def to_json(self) -> Json:
-        data = get_template(self.template)
-        data["type"]["metadata"]["title"] = self.title
+        data = get_template(type(self).__name__)
         data["type"]["subContentId"] = str(self.subcontent_id)
         return data
 
+
+@dataclass
+class OuterElement(Element):
+    title: str
+    index: int | None = field(default=None, init=False)
+
+    def to_json(self) -> Json:
+        data = super().to_json()
+        data["type"]["metadata"]["title"] = self.title
+        return data
+
     def build_task(self) -> str:
-        questions: dict[UUID, Question] = {}
-        found: list[Question] = [self]
+        questions: dict[UUID, OuterElement] = {}
+        found: list[OuterElement] = [self]
         while found:
             curr = found.pop()
             questions[curr.subcontent_id] = curr
-            if isinstance(curr, InPresentation) and curr.next_question is not None:
+            if isinstance(curr, Presentation) and curr.next_question is not None:
                 found.append(curr.next_question)
             elif isinstance(curr, BranchingQuestion):
                 found.extend(alternative.next_question for alternative in curr.alternatives)
@@ -68,25 +83,38 @@ class Question:
         data["branchingScenario"]["content"] = [question.to_json() for question in question_list]
         return json.dumps(data)
 
+    def package_task(self, out: Path) -> None:
+        if out.exists():
+            out.unlink()
+        template = Path(__file__).parent / "templates" / "template.h5p"
+        template.copy(out)
+        with ZipFile(out, mode="a", compresslevel=9) as zip:
+            zip.writestr("content/content.json", self.build_task())
+
 
 @dataclass
-class InPresentation(Question, ABC):
-    next_question: Question | None = field(default=None, kw_only=True)
+class PresentationElement(Element):
+    x: int
+    y: int
+    width: int
+    height: int
 
-    template: ClassVar = "presentation"
+
+@dataclass
+class Presentation(OuterElement):
+    inner_elements: list[PresentationElement] = field(default_factory=list[PresentationElement])
+    next_question: OuterElement | None = field(default=None, kw_only=True)
 
     def to_json(self) -> Json:
         data = super().to_json()
         data["nextContentId"] = self.next_question.index if self.next_question is not None else -1
-        inner = self.inner_json()
-        width = 100 // len(inner)
         data["type"]["params"]["presentation"]["slides"][0]["elements"] = [
             {
-                "x": x,
-                "y": 0,
-                "width": width,
-                "height": 100,
-                "action": obj,
+                "x": elem.x,
+                "y": elem.y,
+                "width": elem.width,
+                "height": elem.height,
+                "action": elem.to_json(),
                 "alwaysDisplayComments": False,
                 "backgroundOpacity": 0,
                 "displayAsButton": False,
@@ -95,12 +123,9 @@ class InPresentation(Question, ABC):
                 "invisible": False,
                 "solution": "",
             }
-            for (obj, x) in zip(inner, range(0, 100, width), strict=True)
+            for elem in self.inner_elements
         ]
         return data
-
-    @abstractmethod
-    def inner_json(self) -> list[Json]: ...
 
 
 @dataclass
@@ -121,33 +146,25 @@ class MultipleChoiceAnswer:
 
 
 @dataclass
-class MultipleChoiceQuestion(InPresentation):
+class MultipleChoiceQuestion(PresentationElement):
     question: str
     answers: list[MultipleChoiceAnswer]
 
-    def inner_json(self) -> list[Json]:
-        if len(self.answers) > 8:
-            first, second = split_text(self.question)
-            data = [(first, self.answers[: len(self.answers) // 2]), (second, self.answers[len(self.answers) // 2 :])]
-        else:
-            data = [(self.question, self.answers)]
-        questions: list[Json] = []
-        for question, answers in data:
-            multi_choice = get_template("multipleChoiceQuestion")
-            multi_choice["params"]["question"] = format_text(question)
-            multi_choice["params"]["answers"] = [answer.to_json() for answer in answers]
-            questions.append(multi_choice)
-        return questions
+    def to_json(self) -> Json:
+        data = super().to_json()
+        data["params"]["question"] = format_text(self.question)
+        data["params"]["answers"] = [answer.to_json() for answer in self.answers]
+        return data
 
 
 @dataclass
-class Blanks(InPresentation):
+class Blanks(PresentationElement):
     description: str
     top_text: str
     bottom_text: str
     answers: list[str]
 
-    def inner_json(self) -> list[Json]:
+    def to_json(self) -> Json:
         data = get_template("blanks")
         data["params"]["text"] = format_text(self.description)
         data["params"]["questions"][0] = (
@@ -155,13 +172,13 @@ class Blanks(InPresentation):
             + format_text("*" + "/".join(self.answers) + "*")
             + format_text(self.bottom_text)
         )
-        return [data]
+        return data
 
 
 @dataclass
 class BranchingAlternative:
     text: str
-    next_question: Question
+    next_question: OuterElement
 
     def to_json(self) -> Json:
         return {
@@ -175,7 +192,7 @@ class BranchingAlternative:
 
 
 @dataclass
-class BranchingQuestion(Question):
+class BranchingQuestion(OuterElement):
     question: str
     alternatives: list[BranchingAlternative]
 
@@ -219,4 +236,5 @@ if __name__ == "__main__":
             BranchingAlternative("nope", nope),
         ],
     )
-    Path("content.json").write_text(first.build_task())
+    #bundle_template(Path(__file__).parent / "templates" / "template.h5p")
+    first.package_task(Path("test.h5p"))
